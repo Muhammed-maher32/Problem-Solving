51c421e26b790f0f67797ed10af3845d31bad99f
Mohamed Maher <muhammedmaher916@gmail.com>
2026-04-20 06:13:14 +0200

    Update two-sum-ii-input-array-is-sorted.cs

--- Two-Pointers/two-sum-ii-input-array-is-sorted.cs
+++ Two-Pointers/two-sum-ii-input-array-is-sorted.cs
@@ -1,13 +1,11 @@
 // Problem: 167. Two Sum II - Input Array Is Sorted
 // Author: Muhammed Maher
 // Approach: Two pointers, since the array is sorted in ASC order
-	     we iterate using l and r, if sum > target move r to left to reduce sum.
+	     	//we iterate using l and r, if sum > target move r to left to reduce sum.
 
 // Why r = n-1 is better than r = l+1?  
-
-If we initialize r = l + 1, the approach fails to utilize the "Sorted" property effectively:
-
-Directional Limitation: If the sum is already greater than the target, you cannot move r backwards to find a smaller number because r is already at the smallest possible position relative to l. It takes n square while the first one takes o(n).
+//If we initialize r = l + 1, the approach fails to utilize the "Sorted" property effectively:
+//Directional Limitation: If the sum is already greater than the target, you cannot move r backwards to find a smaller number because r is already at the smallest possible position relative to l. It takes n square while the first one takes o(n).
 
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
